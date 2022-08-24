b37789d315b67d5fa8cbfe022db5d64cb454972f
Tracey Jaquith <tracey@archive.org>
2022-08-23 19:45:53 -0700

    html; better formatting

--- regen.sh
+++ regen.sh
@@ -1,14 +1,30 @@
 #!/bin/zsh -ex
 
+ME=https://tracey.archive.org
+
 (
-  echo '# Slides'
-  echo '## Slides and decks from recent Talks and gatherings'
-  echo
-  echo 'See also: <a href="https://tracey.archive.org/">https://tracey.archive.org</a>'
-  ( for i in $(/bin/ls -d */ |tr -d / |egrep -v '^images$'); do
-      echo -n "[$i](https://traceypooh.github.io/slides/$i)\t"
-      echo -n "<small><i>(last update: "
+  echo "
+<link href='https://esm.archive.org/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet'>
+<style>
+  body { margin: 50px }
+  a { text-decoration:none }
+  img { max-width:100%; height:auto }
+  small { float: right }
+</style>
+
+
+<h1>Tracey Jaquith slides from recent talks</h1>
+
+<p>See also: <a href="$ME">$ME</a></p>
+"
+
+  (
+    for i in $(/bin/ls -d */ |tr -d / |egrep -v '^images$'); do
+      echo -n "<p><small><i>(last update: \t"
       git log -1 --format=%ci $i |cut -b1-7 |tr -d '\n'
-      echo ")</i></small><br>"
+      echo -n ")</i></small>"
+      echo -n "<b><a href='https://traceypooh.github.io/slides/$i'>$i</a></b></p>"
+      echo
     done
-  ) |sort -r -k2,5000 ) >| README.md
+  ) |sort -r -k2,5000 -t '\t'
+) >| index.html
