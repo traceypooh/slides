26b1595fd9ee6568fa6ea709f3bf9f93f263cc49
Tracey Jaquith <tracey@archive.org>
2022-08-23 20:47:37 -0700

    devops subdir index

---
+++ README.md
@@ -0,0 +1,1 @@
+@see [regen.sh](regen.sh)
--- regen.sh
+++ regen.sh
@@ -35,9 +35,25 @@ ME=https://tracey.archive.org
 ) >| index.html
 
 
+
 # copy out public OK slide decks and formatting from another repo
+echo "
+<link href='https://esm.archive.org/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet'>
+<style>
+  body { margin: 50px }
+  a { text-decoration:none }
+  img { max-width:100%; height:auto }
+  small { float: right }
+</style>
+
+<h1>🚂 DevOps Training</h1>
+
+" >| devops/index.html
+
+
 for i in  2021-02-03  2021-02-17  2021-02-24  2021-03-10  2021-03-31  2021-04-07  2021-05-12; do
   rsync -Pav  ../TechDocs/devops/$i.md  devops/$i/index.html
+  echo "<p><a href='$i/index.html'>$i</a></p>" >> devops/index.html
 done
 
 rsync -Pav ../TechDocs/eveal.js/ devops/eveal.js/
